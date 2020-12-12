b40de8007a958b1c6b9f41ac89660e9076db59ef
Runar <runar.flaten@zalando.de>
2020-12-12 11:42:32 +0100

    enable cors

--- src/main/java/xyz/runar/chat/SavedMessagesController.java
+++ src/main/java/xyz/runar/chat/SavedMessagesController.java
@@ -1,6 +1,7 @@
 package xyz.runar.chat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class SavedMessagesController {
     @Autowired
     SavedMessagesRepository savedMessagesRepository;
 
+    @CrossOrigin
     @GetMapping("/saved-messages")
     public List<SavedMessage> getSavedMessages() {
         return savedMessagesRepository.getSavedMessages();
